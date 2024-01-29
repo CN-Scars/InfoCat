40f6d8b1661bd5e590fb85b71e6672d548517b54
CN-Scars <yss2813483011xxl@gmail.com>
2024-01-29 15:37:10 +0800

    Add comments to the code of gateway_mac_finder.cpp

--- windows/runner/gateway_mac_finder.cpp
+++ windows/runner/gateway_mac_finder.cpp
@@ -15,23 +15,26 @@
 std::string getGatewayMACAddress(const std::string &gatewayAddress)
 {
     ULONG bufferSize = 0;
+    // 第一次调用，获取缓冲区大小
     GetIpNetTable(nullptr, &bufferSize, FALSE);
 
     std::vector<unsigned char> buffer(bufferSize);
 
+    // 获取ARP表
     if (GetIpNetTable(reinterpret_cast<PMIB_IPNETTABLE>(buffer.data()), &bufferSize, FALSE) ==
         NO_ERROR)
     {
         auto arpTable = reinterpret_cast<PMIB_IPNETTABLE>(buffer.data());
 
+        // 遍历ARP表
         for (int i = 0; i < static_cast<int>(arpTable->dwNumEntries); ++i)
         {
             auto &row = arpTable->table[i];
-            in_addr addr;
-            inet_pton(AF_INET, gatewayAddress.c_str(), &addr);
-            if (row.dwAddr == addr.S_un.S_addr)
+            in_addr addr;   // IPv4地址结构体
+            inet_pton(AF_INET, gatewayAddress.c_str(), &addr);  // 将字符串IP地址转换为in_addr结构体
+            if (row.dwAddr == addr.S_un.S_addr) // 检查是否是网关地址
             {
-
+                // 将MAC地址转换为字符串
                 std::stringstream macStream;
                 macStream << std::hex << std::uppercase
                           << static_cast<int>(row.bPhysAddr[0]) << ":"
@@ -44,5 +47,6 @@ std::string getGatewayMACAddress(const std::string &gatewayAddress)
             }
         }
     }
+    // 没有找到网关的MAC地址
     return std::string();
 }
